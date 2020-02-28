795c9dea6d6839e5775124771c07262e3cb09c0c
Tyler Morgan <tylermorgan210@gmail.com>
2020-02-28 16:01:38 -0500

    Added preferences

---
+++ Filevault Reissue/Filevault Reissue/Models/Preferences.swift
@@ -0,0 +1,47 @@
+//
+//  Preferences.swift
+//  Filevault Reissue
+//
+//  Created by Tyler Morgan on 2/28/20.
+//  Copyright © 2020 Tyler Morgan. All rights reserved.
+//
+
+import Foundation
+
+class Preferences {
+    
+    let sharedInstance = Preferences()
+    private let userDefaults: UserDefaults
+    
+    internal var viewTitle: String? {
+        get {
+            return self.userDefaults.string(forKey: "viewTitle")
+        }
+    }
+    
+    internal var viewInstructions: String? {
+        get {
+            return self.userDefaults.string(forKey: "viewInstructions")
+        }
+    }
+    
+    internal var viewLogoPath: String? {
+        get {
+            return self.userDefaults.string(forKey: "viewLogoPath")
+        }
+    }
+    
+    internal var infoURL: String? {
+        get {
+            return self.userDefaults.string(forKey: "infoURL")
+        }
+    }
+    
+    init(nsUserDefaults: UserDefaults = UserDefaults.standard) {
+        self.userDefaults = nsUserDefaults
+    }
+    
+    init(defaults: UserDefaults) {
+        self.userDefaults = defaults
+    }
+}
